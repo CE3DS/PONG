25d44e497a46f73663f48290e0fded45c06366f7
Carlos Eduardo de Souza e Silva <carlos.silva@grupoarizona.local>
2021-01-05 13:12:19 -0300

    First commit

---
+++ Pong/PongForms.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace Pong
+{
+    partial class Pong
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.NPCBox = new System.Windows.Forms.PictureBox();
+            this.PlayerBox = new System.Windows.Forms.PictureBox();
+            this.Target = new System.Windows.Forms.PictureBox();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.tmMovement = new System.Windows.Forms.Timer(this.components);
+            this.tmTarget = new System.Windows.Forms.Timer(this.components);
+            this.tmNPC = new System.Windows.Forms.Timer(this.components);
+            this.lbPlayerScore = new System.Windows.Forms.Label();
+            this.lbNPCScore = new System.Windows.Forms.Label();
+            this.lblResetScore = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.configuraçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mudarCoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HUDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clrBackground = new System.Windows.Forms.ColorDialog();
+            this.clrHUD = new System.Windows.Forms.ColorDialog();
+            this.clrPlayer = new System.Windows.Forms.ColorDialog();
+            this.tmBackgroundColor = new System.Windows.Forms.Timer(this.components);
+            this.tmHUDColor = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.NPCBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Target)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NPCBox
+            // 
+            this.NPCBox.BackColor = System.Drawing.Color.Black;
+            this.NPCBox.Location = new System.Drawing.Point(700, 200);
+            this.NPCBox.Name = "NPCBox";
+            this.NPCBox.Size = new System.Drawing.Size(20, 100);
+            this.NPCBox.TabIndex = 0;
+            this.NPCBox.TabStop = false;
+            // 
+            // PlayerBox
+            // 
+            this.PlayerBox.BackColor = System.Drawing.Color.Black;
+            this.PlayerBox.Location = new System.Drawing.Point(100, 200);
+            this.PlayerBox.Name = "PlayerBox";
+            this.PlayerBox.Size = new System.Drawing.Size(20, 100);
+            this.PlayerBox.TabIndex = 1;
+            this.PlayerBox.TabStop = false;
+            this.PlayerBox.LocationChanged += new System.EventHandler(this.PlayerBox_LocationChanged);
+            // 
+            // Target
+            // 
+            this.Target.BackColor = System.Drawing.Color.Black;
+            this.Target.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Target.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Target.Location = new System.Drawing.Point(390, 240);
+            this.Target.Name = "Target";
+            this.Target.Size = new System.Drawing.Size(20, 20);
+            this.Target.TabIndex = 2;
+            this.Target.TabStop = false;
+            this.Target.LocationChanged += new System.EventHandler(this.Target_LocationChanged);
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Font = new System.Drawing.Font("Segoe Print", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.Location = new System.Drawing.Point(264, 150);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(273, 62);
+            this.lblStart.TabIndex = 3;
+            this.lblStart.Text = "Move to Start";
+            // 
+            // tmMovement
+            // 
+            this.tmMovement.Interval = 10;
+            this.tmMovement.Tick += new System.EventHandler(this.movementTimer_Tick);
+            // 
+            // tmTarget
+            // 
+            this.tmTarget.Interval = 10;
+            this.tmTarget.Tick += new System.EventHandler(this.tmTarget_Tick);
+            // 
+            // tmNPC
+            // 
+            this.tmNPC.Interval = 10;
+            this.tmNPC.Tick += new System.EventHandler(this.tmNPC_Tick);
+            // 
+            // lbPlayerScore
+            // 
+            this.lbPlayerScore.AutoSize = true;
+            this.lbPlayerScore.Font = new System.Drawing.Font("Segoe Print", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPlayerScore.Location = new System.Drawing.Point(12, 35);
+            this.lbPlayerScore.Name = "lbPlayerScore";
+            this.lbPlayerScore.Size = new System.Drawing.Size(60, 47);
+            this.lbPlayerScore.TabIndex = 10;
+            this.lbPlayerScore.Text = "00";
+            // 
+            // lbNPCScore
+            // 
+            this.lbNPCScore.AutoSize = true;
+            this.lbNPCScore.Font = new System.Drawing.Font("Segoe Print", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNPCScore.Location = new System.Drawing.Point(728, 35);
+            this.lbNPCScore.Name = "lbNPCScore";
+            this.lbNPCScore.Size = new System.Drawing.Size(60, 47);
+            this.lbNPCScore.TabIndex = 11;
+            this.lbNPCScore.Text = "00";
+            // 
+            // lblResetScore
+            // 
+            this.lblResetScore.AutoSize = true;
+            this.lblResetScore.Font = new System.Drawing.Font("Segoe Print", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResetScore.Location = new System.Drawing.Point(329, 200);
+            this.lblResetScore.Name = "lblResetScore";
+            this.lblResetScore.Size = new System.Drawing.Size(147, 21);
+            this.lblResetScore.TabIndex = 12;
+            this.lblResetScore.Text = "or press \"R\" to restart";
+            this.lblResetScore.Visible = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configuraçõesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 29);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // configuraçõesToolStripMenuItem
+            // 
+            this.configuraçõesToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.configuraçõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mudarCoresToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.configuraçõesToolStripMenuItem.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.configuraçõesToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.configuraçõesToolStripMenuItem.Name = "configuraçõesToolStripMenuItem";
+            this.configuraçõesToolStripMenuItem.Size = new System.Drawing.Size(64, 25);
+            this.configuraçõesToolStripMenuItem.Text = "Opções";
+            // 
+            // mudarCoresToolStripMenuItem
+            // 
+            this.mudarCoresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backgroundToolStripMenuItem,
+            this.HUDToolStripMenuItem});
+            this.mudarCoresToolStripMenuItem.Name = "mudarCoresToolStripMenuItem";
+            this.mudarCoresToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.mudarCoresToolStripMenuItem.Text = "Mudar Cores";
+            // 
+            // backgroundToolStripMenuItem
+            // 
+            this.backgroundToolStripMenuItem.Name = "backgroundToolStripMenuItem";
+            this.backgroundToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.backgroundToolStripMenuItem.Text = "Background";
+            this.backgroundToolStripMenuItem.Click += new System.EventHandler(this.backgroundToolStripMenuItem_Click);
+            // 
+            // HUDToolStripMenuItem
+            // 
+            this.HUDToolStripMenuItem.Name = "HUDToolStripMenuItem";
+            this.HUDToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.HUDToolStripMenuItem.Text = "HUD";
+            this.HUDToolStripMenuItem.Click += new System.EventHandler(this.HUDToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(156, 26);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // tmBackgroundColor
+            // 
+            this.tmBackgroundColor.Tick += new System.EventHandler(this.tmBackgroundColor_Tick);
+            // 
+            // tmHUDColor
+            // 
+            this.tmHUDColor.Tick += new System.EventHandler(this.tmHUDColor_Tick);
+            // 
+            // Pong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.lblResetScore);
+            this.Controls.Add(this.lbNPCScore);
+            this.Controls.Add(this.lbPlayerScore);
+            this.Controls.Add(this.PlayerBox);
+            this.Controls.Add(this.NPCBox);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.Target);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Pong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.NPCBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Target)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox NPCBox;
+        private System.Windows.Forms.PictureBox PlayerBox;
+        private System.Windows.Forms.PictureBox Target;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Timer tmMovement;
+        private System.Windows.Forms.Timer tmTarget;
+        private System.Windows.Forms.Timer tmNPC;
+        private System.Windows.Forms.Label lbPlayerScore;
+        private System.Windows.Forms.Label lbNPCScore;
+        private System.Windows.Forms.Label lblResetScore;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem configuraçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mudarCoresToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog clrBackground;
+        private System.Windows.Forms.ColorDialog clrHUD;
+        private System.Windows.Forms.ToolStripMenuItem backgroundToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HUDToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog clrPlayer;
+        private System.Windows.Forms.Timer tmBackgroundColor;
+        private System.Windows.Forms.Timer tmHUDColor;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
---
+++ Pong/PongForms.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Pong
+{
+    public partial class Pong : Form
+    {
+        Random random = new Random();
+
+        private bool hitUpBorder = false;
+        private bool hitDownBorder = false;
+        private bool running = false;
+
+        private bool moveUp = false;
+        private bool moveDown = false;
+
+        private bool goingLeft;
+        private bool goingUp;
+
+        private int playerScore = 0;
+        private int NPCScore = 0;
+
+        private int targetSpeedX = 10;
+        private int targetSpeedY = 1;
+
+        public Pong()
+        {
+            InitializeComponent();
+        }
+
+        private void Running()
+        {
+            if (running == true)
+            {
+                tmBackgroundColor.Stop();
+                tmHUDColor.Stop();
+                lblResetScore.Visible = false;
+                lblStart.Visible = false;
+                tmTarget.Enabled = true;
+                tmNPC.Enabled = true;
+                goingLeft = false;
+                goingUp = false;
+            }
+            else
+            {
+                if ((playerScore > 0) || (NPCScore > 0))
+                {
+                    lblResetScore.Visible = true;
+                }
+                tmMovement.Stop();
+                tmNPC.Stop();
+                tmTarget.Stop();
+
+                PlayerBox.Location = new Point(100, 200);
+                NPCBox.Location = new Point(700, 200);
+                Target.Location = new Point(390, 240);
+
+                lblStart.Visible = true;
+            }
+        }
+
+        private bool GetDirection(int targetY, int boxY)
+        {
+            bool boolean = true;
+            if (targetY + 9 < boxY + 49)
+                boolean = true;
+            else boolean = false;
+
+            int x = random.Next(0, 5);
+            if (x > 3)
+            {
+                if (boolean == true) boolean = false;
+                else boolean = true;
+            }
+            return boolean;
+        }
+
+        private void ChangeAngle(int targetY, int boxY)
+        {
+            if ((targetY + 9 > boxY + 24) && (targetY + 9 < boxY + 74))
+                targetSpeedY = 1;
+            else targetSpeedY = 3;
+
+        }
+
+        private void refreshScore()
+        {
+            lbNPCScore.Text = NPCScore.ToString("00");
+            lbPlayerScore.Text = playerScore.ToString("00");
+        }
+
+        private void backgroundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clrBackground.ShowDialog();
+            tmBackgroundColor.Start();
+        }
+
+        private void HUDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clrHUD.ShowDialog();
+            tmHUDColor.Start();
+        }
+
+        private void tmBackgroundColor_Tick(object sender, EventArgs e)
+        {
+            this.BackColor = clrBackground.Color;
+        }
+
+        private void tmHUDColor_Tick(object sender, EventArgs e)
+        {
+            lbNPCScore.ForeColor = clrHUD.Color;
+            lbPlayerScore.ForeColor = clrHUD.Color;
+            lblStart.ForeColor = clrHUD.Color;
+            lblResetScore.ForeColor = clrHUD.Color;
+            Target.BackColor = clrHUD.Color;
+            NPCBox.BackColor = clrHUD.Color;
+            PlayerBox.BackColor = clrHUD.Color;
+        }
+
+        
+        // Target -------------------------------------------------------------------
+        private void tmTarget_Tick(object sender, EventArgs e)
+        {
+            if (goingLeft == true && goingUp == true)
+            {
+                Target.Location = new Point(Target.Location.X - targetSpeedX, Target.Location.Y - targetSpeedY);
+                return;
+            }
+            if (goingLeft == true && goingUp == false)
+            {
+                Target.Location = new Point(Target.Location.X - targetSpeedX, Target.Location.Y + targetSpeedY);
+                return;
+            }
+            if (goingLeft == false && goingUp == true)
+            {
+                Target.Location = new Point(Target.Location.X + targetSpeedX, Target.Location.Y - targetSpeedY);
+                return;
+            }
+            if (goingLeft == false && goingUp == false)
+            {
+                Target.Location = new Point(Target.Location.X + targetSpeedX, Target.Location.Y + targetSpeedY);
+                return;
+            }
+        }
+
+        private void Target_LocationChanged(object sender, EventArgs e)
+        {
+            // Bater em cima
+            if (Target.Location.Y <= 50)
+            {
+                goingUp = false;
+            }
+            // Bater em baixo
+            else if (Target.Location.Y >= 430)
+            {
+                goingUp = true;
+            }
+
+            // Bater no player
+            if ((Target.Location.X <= PlayerBox.Location.X + 18)
+                && (Target.Location.X >= PlayerBox.Location.X + 10)
+                && (Target.Location.Y > PlayerBox.Location.Y - 17)
+                && (Target.Location.Y < PlayerBox.Location.Y + 99))
+            {
+                ChangeAngle(Target.Location.Y, PlayerBox.Location.Y);
+                goingLeft = false;
+                goingUp = GetDirection(Target.Location.Y, PlayerBox.Location.Y);
+            }
+
+            // Bater no oponente
+            else if ((Target.Location.X >= NPCBox.Location.X - 18)
+                && (Target.Location.X <= NPCBox.Location.X - 10)
+                && (Target.Location.Y > NPCBox.Location.Y - 17)
+                && (Target.Location.Y < NPCBox.Location.Y + 99))
+            {
+                ChangeAngle(Target.Location.Y, NPCBox.Location.Y);
+                goingLeft = true;
+                goingUp = GetDirection(Target.Location.Y, NPCBox.Location.Y);
+            }
+
+            // Ponto NPC
+            else if (Target.Location.X <= 20)
+            {
+                running = false;
+                NPCScore++;
+                refreshScore();
+                Running();
+            }
+
+            // Ponto Player
+            else if (Target.Location.X >= 760)
+            {
+                running = false;
+                playerScore++;
+                refreshScore();
+                Running();
+            }
+        }
+
+
+        // Player ---------------------------------------------------
+        private void PlayerBox_LocationChanged(object sender, EventArgs e)
+        {
+            if (PlayerBox.Location.Y <= 50)
+                hitUpBorder = true;
+            else hitUpBorder = false;
+
+            if (PlayerBox.Location.Y >= 350)
+                hitDownBorder = true;
+            else hitDownBorder = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.R:
+                    if (running == false)
+                    {
+                        playerScore = 0;
+                        NPCScore = 0;
+                        lblResetScore.Visible = false;
+                        refreshScore();
+                    }
+                    break;
+
+                case Keys.Up:
+                    if (running == false)
+                    {
+                        running = true;
+                        Running();
+                    }
+                    moveUp = true;
+                    break;
+
+                case Keys.Down:
+                    if (running == false)
+                    {
+                        running = true;
+                        Running();
+                    }
+                    moveDown = true;
+                    break;
+
+                case Keys.Escape:
+                    this.Close();
+                    break;
+            }
+
+            Move();
+            tmMovement.Start();
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    moveUp = false;
+                    break;
+
+                case Keys.Down:
+                    moveDown = false;
+                    break;
+            }
+
+            if (!(moveUp || moveDown))
+            {
+                tmMovement.Stop();
+            }
+        }
+
+        private void MoveUp()
+        {
+            if (hitUpBorder == false && moveUp == true)
+            {
+                PlayerBox.Location = new Point(PlayerBox.Location.X, PlayerBox.Location.Y - 5);
+            }
+        }
+        private void MoveDown()
+        {
+            if (hitDownBorder == false && moveDown == true)
+            {
+                PlayerBox.Location = new Point(PlayerBox.Location.X, PlayerBox.Location.Y + 5);
+            }
+        }
+
+        private void movementTimer_Tick(object sender, EventArgs e)
+        {
+            Move();
+        }
+        private new void Move()
+        {
+            if (moveUp == true) MoveUp();
+            else if (moveDown == true) MoveDown();
+        }
+
+
+
+        // NPC
+        private void tmNPC_Tick(object sender, EventArgs e)
+        {
+            if ((Target.Location.Y + 9 > NPCBox.Location.Y + 49) && (NPCBox.Location.Y < 350))
+            {
+                NPCBox.Location = new Point(NPCBox.Location.X, NPCBox.Location.Y + 3);
+            }
+            else if ((Target.Location.Y + 9 < NPCBox.Location.Y + 49) && (NPCBox.Location.Y > 50))
+            {
+                NPCBox.Location = new Point(NPCBox.Location.X, NPCBox.Location.Y - 3);
+            }
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
